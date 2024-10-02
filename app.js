34d113a466febcb80df6110bbcb52e5c588a4e64
Adefranky <155418329+Adefranky@users.noreply.github.com>
2024-10-02 16:18:59 +0300

    event listeners

--- app.js
+++ app.js
@@ -80,3 +80,26 @@ const newList = document.querySelector('.new-list')
 const fourth = document.querySelector('.fourth')
 newList.removeChild("fourth")
 newList.remove() // removes all
+
+// Events and eventListener
+
+const secondBtn = document.querySelector(".second-btn");
+secondBtn.onclick = function(){
+    console.log("Frank")
+}
+
+const best = document.querySelector('.best')
+best.addEventListener("click", function() {
+    console.log("Hello World");
+});
+
+// arrow function
+best.addEventListener("click",() =>{
+    console.log("Yellow");
+})
+
+// Event object
+const para = document.querySelector('.para')
+para.addEventListener('click', (event) =>{
+    console.log(event)
+})
