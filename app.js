4119389eba500827ffc85d3a2a340642371c12d2
Adefranky <155418329+Adefranky@users.noreply.github.com>
2024-10-03 14:23:17 +0300

    changes made

--- app.js
+++ app.js
@@ -128,3 +128,33 @@ put.addEventListener("keydown", ()=>{
     console.log("keydown");
 });
 
+// USEFUL PROPERTIES AND METHODS
+input.addEventListener("keypress", (e)=>{
+    console.log(e.charCode);
+    console.log(e.Code);
+    console.log(e.CtrlKey);
+    console.log(e.shiftKey);
+})
+
+// Calculator project
+document.getElementById("btn").addEventListener("click", ()=>{
+    let num1 = document.querySelector(".num1").value;
+    let num2 = document.querySelector('.num2').value;
+    let result = document.querySelector('.result');
+    let operator = document.getElementById("selectOp").value;
+
+    switch(operator){
+        case "plus":
+            result.innerHtml = Number(num1) + Number(num2);
+            break;
+            case "min":
+                result.innerHTML = Number(num1) - Number(num2);
+                break;
+                case "dev": 
+                result.innerHTML = Number(num1) / Number (num2);
+                break;
+                case "multi":
+                    result.innerHTML = Number(num1) * Number(num2);
+                    break;
+    }
+});
